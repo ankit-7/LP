5b04b50eb668a5bc4671bfc0393ac007ea2ef54b
ankit-7 <84506602+ankit-7@users.noreply.github.com>
2023-05-25 08:05:12 +0530

    Add files via upload

--- Bubble Sort.cpp
+++ Bubble Sort.cpp
@@ -1,65 +1,87 @@
 #include <iostream>
-#include <chrono>
+#include <vector>
 #include <omp.h>
+using namespace std;
 
-void bubbleSortParallel(int* arr, int size) {
-    #pragma omp parallel
+void bubble_sort_sequential(vector<int>& arr) {
+    int n = arr.size();
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+void bubble_sort_parallel(vector<int>& arr) {
+    int n = arr.size();
+    int phase, start;
+
+    #pragma omp parallel shared(arr, n) private(phase, start)
     {
-        for (int i = 0; i < size - 1; ++i) {
-            #pragma omp for
-            for (int j = 0; j < size - i - 1; ++j) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+        for (phase = 0; phase < n; phase++) {
+            if (phase % 2 == 0) {  // Even phase
+                #pragma omp for schedule(static)
+                for (start = 0; start < n; start += 2) {
+                    if (arr[start] > arr[start + 1]) {
+                        swap(arr[start], arr[start + 1]);
+                    }
+                }
+            } else {  // Odd phase
+                #pragma omp for schedule(static)
+                for (start = 1; start < n - 1; start += 2) {
+                    if (arr[start] > arr[start + 1]) {
+                        swap(arr[start], arr[start + 1]);
+                    }
                 }
             }
-        }
-    }
-}
 
-void bubbleSortSequential(int* arr, int size) {
-    for (int i = 0; i < size - 1; ++i) {
-        for (int j = 0; j < size - i - 1; ++j) {
-            if (arr[j] > arr[j + 1]) {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+            #pragma omp barrier
+
+            #pragma omp master
+            {
+                cout << "Phase " << phase << ": ";
+                for (int num : arr) {
+                    cout << num << " ";
+                }
+                cout << endl;
             }
         }
     }
 }
 
-void printArray(int* arr, int size) {
-    for (int i = 0; i < size; ++i) {
-        std::cout << arr[i] << " ";
+void print_array(const vector<int>& arr) {
+    for (int num : arr) {
+        cout << num << " ";
     }
-    std::cout << std::endl;
+    cout << endl;
 }
 
 int main() {
-    const int size = 10000;
-    int arr[size];
-    for (int i = 0; i < size; ++i) {
-        arr[i] = size - i;
-    }
+    vector<int> arr = {9, 5, 7, 1, 3, 2, 8, 6, 4};
+    int n = arr.size();
 
-    // Parallel Bubble Sort
-    auto start = std::chrono::high_resolution_clock::now();
-    bubbleSortParallel(arr, size);
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> duration = end - start;
-    std::cout << "Parallel Bubble Sort Time: " << duration.count() << " seconds" << std::endl;
+    cout << "Original Array: ";
+    print_array(arr);
 
-    // Sequential Bubble Sort
-    for (int i = 0; i < size; ++i) {
-        arr[i] = size - i;
-    }
-    start = std::chrono::high_resolution_clock::now();
-    bubbleSortSequential(arr, size);
-    end = std::chrono::high_resolution_clock::now();
-    duration = end - start;
-    std::cout << "Sequential Bubble Sort Time: " << duration.count() << " seconds" << std::endl;
+    vector<int> arr_seq = arr;
+    double start_time_seq = omp_get_wtime();
+    bubble_sort_sequential(arr_seq);
+    double end_time_seq = omp_get_wtime();
+
+    cout << "Sorted Array (Sequential): ";
+    print_array(arr_seq);
+    cout << "Time Taken (Sequential): " << end_time_seq - start_time_seq << " seconds" << endl;
+
+    vector<int> arr_par = arr;
+    double start_time_par = omp_get_wtime();
+    bubble_sort_parallel(arr_par);
+    double end_time_par = omp_get_wtime();
+
+    cout << "Sorted Array (Parallel): ";
+    print_array(arr_par);
+    cout << "Time Taken (Parallel): " << end_time_par - start_time_par << " seconds" << endl;
 
     return 0;
 }
